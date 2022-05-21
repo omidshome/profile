a5488bde9f1c1f31e41dab04d369eb57327bc0dc
unknown <omidshome@gmail.com>
2022-05-21 20:52:22 +0430

    creating card page

---
+++ pages/cards/index.js
@@ -0,0 +1,14 @@
+const index = () => {
+  return (
+    <div style={{ direction: "rtl" }}>
+      <div>
+        <h2>:عنوان</h2>
+        <p>آیا میخواهید این کارت را حذف کنید؟</p>
+        <button>بله</button>
+        <button>خیر</button>
+      </div>
+    </div>
+  );
+};
+
+export default index;
