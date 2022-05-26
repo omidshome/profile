0c73ae1a4595b98d9ab9704dc635d94fbf82ba7f
unknown <omidshome@gmail.com>
2022-05-26 22:44:33 +0430

    data Grid successfully displayed with Icons

--- components/cryptoGrid/CryptoGrids.js
+++ components/cryptoGrid/CryptoGrids.js
@@ -1,15 +1,19 @@
 import { ControlPointDuplicate } from "@mui/icons-material";
 import { Typography } from "@mui/material";
-import { Box, Container } from "@mui/system";
+import { Box, Container, width } from "@mui/system";
 import { DataGrid } from "@mui/x-data-grid";
+import Image from "next/image";
 import React from "react";
 
 function CryptoGrids({ data }) {
   const coinRows = data.map((coin) => {
-    return { id: coin.id, CoinName: coin.name, Price: coin.current_price };
+    return {
+      id: coin.id,
+      CoinName: coin.name,
+      Price: coin.current_price,
+      Icon: coin.image,
+    };
   });
-
-  console.log("**************", { coinRows });
 
   return (
     <Container style={{ height: 250, width: "100%" }}>
@@ -19,7 +23,15 @@ function CryptoGrids({ data }) {
 
       <DataGrid
         columns={[
-          { field: "coinImage", headerName: "", width: 70 },
+          {
+            field: "Icon",
+            headerName: "Icon",
+            width: 100,
+            renderCell: (value) => {
+              console.log("*****", value.row.Icon);
+              return <img src={value.row.Icon} style={{ width: "30px" }} />;
+            },
+          },
           { field: "CoinName", headerName: "Coin", width: 150 },
           { field: "Price", headerName: "Coin Price", width: 150 },
         ]}
@@ -30,3 +42,7 @@ function CryptoGrids({ data }) {
 }
 
 export default CryptoGrids;
+
+// export const imageLoader = ({ src }) => {
+//   return <image alt="Crypto Coin" src={`${src}`} style={{ width: "100px" }} />;
+// };
