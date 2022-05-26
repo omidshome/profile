118602c364ba32b1af3bae310d1347315f0b0632
unknown <omidshome@gmail.com>
2022-05-26 14:56:39 +0430

    installing grid data from MUI

--- pages/cryptot/index.js
+++ pages/cryptot/index.js
@@ -1,7 +1,21 @@
 import React from "react";
+import axios from "axios";
 
-const Crypto = () => {
+const Crypto = ({ data }) => {
   return <h1 style={{ paddingTop: "3rem" }}>Cryptocurrency Tracker</h1>;
 };
 
 export default Crypto;
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24"
+  );
+
+  const data = await response.json();
+  console.log({ data });
+
+  return {
+    props: { data: data },
+  };
+}
