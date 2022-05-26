3ba4eb7a0ad32d9867cd6009517e6719386a27b7
unknown <omidshome@gmail.com>
2022-05-26 23:03:24 +0430

    koulen font added

--- components/cryptoGrid/CryptoGrids.js
+++ components/cryptoGrid/CryptoGrids.js
@@ -2,7 +2,6 @@ import { ControlPointDuplicate } from "@mui/icons-material";
 import { Typography } from "@mui/material";
 import { Box, Container, width } from "@mui/system";
 import { DataGrid } from "@mui/x-data-grid";
-import Image from "next/image";
 import React from "react";
 
 function CryptoGrids({ data }) {
@@ -12,28 +11,35 @@ function CryptoGrids({ data }) {
       CoinName: coin.name,
       Price: coin.current_price,
       Icon: coin.image,
+      change: coin.market_cap_change_percentage_24h,
     };
   });
 
   return (
-    <Container style={{ height: 250, width: "100%" }}>
-      <Typography variant="h3" component={"h1"} paddingTop={"4rem"}>
+    <Container style={{ height: "80vh", width: "100%" }}>
+      <Typography
+        variant="h3"
+        component={"h1"}
+        paddingTop={"5rem"}
+        marginBottom={"2.5rem"}
+      >
         My favorit Cryptocurrency-Tracker
       </Typography>
 
       <DataGrid
+        pageSize={7}
         columns={[
           {
             field: "Icon",
             headerName: "Icon",
             width: 100,
             renderCell: (value) => {
-              console.log("*****", value.row.Icon);
               return <img src={value.row.Icon} style={{ width: "30px" }} />;
             },
           },
           { field: "CoinName", headerName: "Coin", width: 150 },
           { field: "Price", headerName: "Coin Price", width: 150 },
+          { field: "change", headerName: "Last 24 change", width: 300 },
         ]}
         rows={coinRows}
       />
