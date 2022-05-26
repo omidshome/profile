7ed329f84dd3c76abc7446b20ec709247e96647a
unknown <omidshome@gmail.com>
2022-05-26 23:30:32 +0430

    adding symbol column and rows

--- components/cryptoGrid/CryptoGrids.js
+++ components/cryptoGrid/CryptoGrids.js
@@ -1,6 +1,6 @@
 import { ControlPointDuplicate } from "@mui/icons-material";
 import { Typography } from "@mui/material";
-import { Box, Container, width } from "@mui/system";
+import { Box, color, Container, width } from "@mui/system";
 import { DataGrid } from "@mui/x-data-grid";
 import React from "react";
 
@@ -12,11 +12,12 @@ function CryptoGrids({ data }) {
       Price: coin.current_price,
       Icon: coin.image,
       change: coin.market_cap_change_percentage_24h,
+      symbol: coin.symbol,
     };
   });
 
   return (
-    <Container style={{ height: "80vh", width: "100%" }}>
+    <Container style={{ height: "80vh", width: "70%" }}>
       <Typography
         variant="h3"
         component={"h1"}
@@ -28,6 +29,7 @@ function CryptoGrids({ data }) {
       </Typography>
 
       <DataGrid
+        rowHeight={70}
         pageSize={7}
         columns={[
           {
@@ -38,6 +40,7 @@ function CryptoGrids({ data }) {
               return <img src={value.row.Icon} style={{ width: "30px" }} />;
             },
           },
+          { field: "symbol", headerName: "Symbol", width: 150 },
           { field: "CoinName", headerName: "Coin", width: 150 },
           { field: "Price", headerName: "Coin Price", width: 150 },
           { field: "change", headerName: "Last 24 change", width: 300 },
