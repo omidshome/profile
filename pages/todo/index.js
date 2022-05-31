d6008e0f0f046c8303315be7cda8e321406fa709
unknown <omidshome@gmail.com>
2022-05-31 11:34:39 +0430

    Adding bootstrap Container to the page

--- pages/todo/index.js
+++ pages/todo/index.js
@@ -1,17 +1,17 @@
-import { Stack } from "react-bootstrap";
+import { Container, Stack } from "react-bootstrap";
 import ToDoDatePicker from "../../components/todoComponents/ToDoDatePicker";
 
 function Index() {
   return (
-    <div>
-      <div className="mt-5 py-5 px-5">
+    <Container>
+      <div className="mt-5 py-5">
         <h1 className="display-4 " dir="rtl">
           به <span className="fw-bold display-5">Todo List</span> خوش آمادید
         </h1>
       </div>
 
       <ToDoDatePicker />
-    </div>
+    </Container>
   );
 }
 
