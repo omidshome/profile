a27e56bdbef1d57cb741070a1d8e4376ba868178
unknown <omidshome@gmail.com>
2022-05-31 12:13:35 +0430

    Adding English to Arabic function Converstor

--- pages/todo/index.js
+++ pages/todo/index.js
@@ -2,6 +2,14 @@ import { Container, Stack } from "react-bootstrap";
 import ToDoDatePicker from "../../components/todoComponents/ToDoDatePicker";
 
 function Index() {
+  const ConvertToArabicNumbers = (num) => {
+    const arabicNumbers =
+      "\u0660\u0661\u0662\u0663\u0664\u0665\u0666\u0667\u0668\u0669";
+    return new String(num).replace(/[0123456789]/g, (d) => {
+      return arabicNumbers[d];
+    });
+  };
+
   return (
     <Container>
       <div className="mt-5 py-5">
